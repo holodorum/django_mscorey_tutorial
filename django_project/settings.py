5011e789cdd49617c5241b134b47cff7061fdf6e
Bart Dubbeldam <bart@Barts-MacBook-Pro.local>
2022-11-20 09:10:44 +0100

    add static root

--- django_project/settings.py
+++ django_project/settings.py
@@ -132,6 +132,7 @@ AWS_S3_FILE_OVERWRITE = False #If same file gets uploaded we don't want it to be
 AWS_DEFAULT_ACL = None #Current default can cause some issues (whatever it means)
 DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage' #Find more info in the django_storages documentation
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
